3bcd6ef8c44a8ca6436de20d65879a00f102d8cb
Samuel-Quenouille <129738646+Samuel-Quenouille@users.noreply.github.com>
2023-04-11 00:35:32 +0200

    Add files via upload

---
+++ Ruby-2/exo_09.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+user_firstname = gets.chomp
+puts "C'est quoi ton nom de famille ?"
+user_lastname = gets.chomp
+print "Bonjour, #{user_firstname} #{user_lastname} !"
---
+++ Ruby-2/exo_10.rb
@@ -0,0 +1,4 @@
+year_of_2017 = 2017
+puts "Bonjour, quelle est ton année de naissance ?"
+date_birth = gets.chomp.to_i
+puts "En 2017 tu avais : #{year_of_2017 - date_birth} ans"
